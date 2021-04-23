4fa8f535c23ba5eb7622a382468a9bc037c68a28
Niels <info@heimseiten.de>
2021-04-23 22:00:20 +0200

    default changes

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,11 +1,4 @@
 <?php
 
-/*
- * This file is part of mobile Navigation.
- * 
- * (c) heimseiten.de - Webdesign aus Köln 2021 <info@heimseiten.de>
- * @license GPL-3.0-or-later
- * For the full copyright and license information,
- * please view the LICENSE file that was distributed with this source code.
- * @link https://github.com/heimseiten/contao-mobile-navigation-bundle
- */
+$GLOBALS['TL_HEAD'][] = '<script defer src="bundles/heimseitencontaomobilenavigation/ContaoMobileNavigationBundle.js"></script>';
+$GLOBALS['TL_CSS'][] = 'bundles/heimseitencontaomobilenavigation/ContaoMobileNavigationBundle.scss|static';
