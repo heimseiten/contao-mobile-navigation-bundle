8e2c03a98a7d7048e6c4eb7268cbff471617d1af
Niels <info@heimseiten.de>
2022-11-02 12:28:48 +0100

    Delete src/Resources/contao/config directory

--- src/Resources/contao/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$GLOBALS['TL_HEAD'][] = '<script defer src="/bundles/heimseitencontaomobilenavigation/ContaoMobileNavigationBundle.js"></script>';
-$GLOBALS['TL_CSS'][] = '/bundles/heimseitencontaomobilenavigation/ContaoMobileNavigationBundle.scss|static';
